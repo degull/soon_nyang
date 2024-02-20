30962fd356a59a88b66f6058b1cfdca6c833bfdc
degull <skytmdgus99@gmail.com>
2024-02-21 00:34:48 +0900

    질병진단 카메라 추가

---
+++ src/components/Camera/CameraCapture.styled.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const VideoContainer = styled.div`
+  position: relative;
+  margin-bottom: 20px;
+`;
+
+export const StyledVideo = styled.video`
+  width: 100%;
+  border-radius: 8px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 10px;
+`;
+
+export const StyledButton = styled.button`
+  padding: 10px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
--- src/routes/Hospital/Hospital.js
+++ src/routes/Hospital/Hospital.js
@@ -2,16 +2,21 @@ import React from 'react';
 import * as S from './Hospital.styled';
 import Header from '../../components/Header/Header';
 import Menu from '../../components/Menu/Menu';
-
+import CameraCapture from '../../components/Camera/CameraCapture';
 
 const Hospital = () => {
-    return (
-        <S.Wrapper>
-            <Header />
-                <h1>병원정보/질병진단</h1>
-            <Menu />
-        </S.Wrapper>
-    );
+  const handleCapture = (imageData) => {
+    // 여기에서 이미지 데이터를 어떻게 처리할지 로직 추가
+    console.log('캡쳐된 이미지 데이터:', imageData);
+  };
+
+  return (
+    <S.Wrapper>
+      <Header />
+      <CameraCapture onCapture={handleCapture} />
+      <Menu />
+    </S.Wrapper>
+  );
 };
 
 export default Hospital;
