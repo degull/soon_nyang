4837926646e0a9c78653aeea34a6f5c6747c3618
degull <skytmdgus99@gmail.com>
2024-02-21 00:15:51 +0900

    로그인/회원가입 조건부 렌더링

--- src/routes/User/User.js
+++ src/routes/User/User.js
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './User.styled';
 import Header from '../../components/Header/Header';
 import Menu from '../../components/Menu/Menu';
+import { useParams } from 'react-router-dom';
 
 const User = () => {
-    return (
-        <S.Wrapper>
-            <Header />
-                <h1>사용자</h1>
-            <Menu />
-        </S.Wrapper>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { userId } = useParams(); // 사용자 ID를 URL에서 가져옴 (루트 파라미터로 가정)
+
+  const handleLogin = () => {
+    // 로그인 로직을 구현
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    // 로그아웃 로직을 구현
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <S.Wrapper>
+      <Header />
+      {isLoggedIn ? (
+        // 사용자가 로그인한 경우, 사용자 프로필 표시
+        <UserProfile userId={userId} />
+      ) : (
+        // 사용자가 로그인하지 않은 경우, 로그인/회원가입 버튼 표시
+        <LoginSignupButtons />
+      )}
+            <Menu isLoggedIn={isLoggedIn} onLogin={handleLogin} onLogout={handleLogout} />
+
+    </S.Wrapper>
+  );
+};
+
+const UserProfile = ({ userId }) => {
+  return (
+    <div>
+      <h2>사용자 프로필 페이지 - 사용자 ID: {userId}</h2>
+      {/* 다른 프로필 관련 컴포넌트 추가 */}
+    </div>
+  );
+};
+
+const LoginSignupButtons = () => {
+  return (
+    <S.LoginSignupButtonsContainer>
+      <S.CatImage src='/img/cat_01.png'/>
+      <S.InputContainer>
+        <S.InputLabel htmlFor="username"></S.InputLabel>
+        <S.InputField type="text" id="username" placeholder='아이디'/>
+      </S.InputContainer>
+      <S.InputContainer>
+        <S.InputLabel htmlFor="password"></S.InputLabel>
+        <S.InputField type="password" id="password" placeholder='비밀번호' />
+      </S.InputContainer>
+      <S.LoginButton>로그인</S.LoginButton>
+      <S.TextAndSignupContainer>
+        <S.Text>아직 계정이 없으신가요?</S.Text>
+        <S.SignupButton>회원가입</S.SignupButton>
+      </S.TextAndSignupContainer>
+    </S.LoginSignupButtonsContainer>
+  );
 };
 
 export default User;
--- src/routes/User/User.styled.js
+++ src/routes/User/User.styled.js
@@ -6,4 +6,91 @@ export const Wrapper = styled.div`
   align-items: center;
   justify-content: space-between;
   min-height: 100vh;
+  background: #fff;
+`;
+export const UserProfileContainer = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const LoginSignupButtonsContainer = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const CatImage = styled.img`
+  width: 200px;
+  height: auto;
+`;
+
+export const LoginButton = styled.button`
+  background-color: #FFA552;
+  color: #fff;
+  width: 350px;
+  height: 32px;
+  font-size: 11px;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  margin-top: 20px;
+  
+`;
+
+export const TextAndSignupContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+  
+`;
+
+export const Text = styled.div`
+    font-size: 9px;
+    color: #C3B3B3;
+    @font-face {
+      font-family: 'omyu_pretty';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2') format('woff2');
+      font-weight: normal;
+      font-style: normal;
+   }
+`;
+
+export const SignupButton = styled.div`
+  font-size: 10px;
+  cursor: pointer;
+  margin-left: 6px;
+  color: #FF9635;
+  font-weight: bold;
+  @font-face {
+      font-family: 'omyu_pretty';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2') format('woff2');
+      font-weight: normal;
+      font-style: normal;
+   }
+`;
+
+export const UserProfileTitle = styled.h2`
+  color: #333;
+`;
+
+export const InputContainer = styled.div`
+  margin-top: 20px;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+export const InputField = styled.input`
+  width: 350px;
+  padding: 8px;
+  font-size: 14px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: -7px;
+  &::placeholder{
+    font-size: 10px;
+    color: #D5C7C7;
+  }
 `;
