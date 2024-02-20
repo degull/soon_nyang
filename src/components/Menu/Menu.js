a2232547ce5127680a5853bc37f1432533a08e17
degull <skytmdgus99@gmail.com>
2024-02-20 23:24:38 +0900

    menubar 추가 & 반응형 추가

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,7 +6,11 @@ const Menu = () => {
     return (
         <S.Wrapper>
             <S.MenuContainer>
-                dd
+            <S.Icon src="/img/home.png" alt="Home" />
+            <S.Icon src="/img/list.png" alt="List" />
+            <S.Icon src="/img/write.png" alt="Write" />
+            <S.Icon src="/img/hospital.png" alt="Hospital" />
+            <S.Icon src="/img/user.png" alt="User" />
             </S.MenuContainer>
         </S.Wrapper>
     );
--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -1,13 +1,49 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-   width: 100%;
-   height: 60px;
-   background: #ccc;
-   margin: 0 auto;
+  width: 100%;
+  height: 60px;
+  background: #ccc;
+  margin: 0 auto;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  overflow: hidden;
 `;
 
 export const MenuContainer = styled.div`
-   width: 390px;
-	margin: 0px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  max-width: 390px;
+  margin: 0 auto;
+
+  @media (min-width: 390px) {
+    width: 100%;
+    max-width: none;
+    justify-content: space-around; /* Equal spacing between icons */
+  }
+
+  @media (min-width: 600px) {
+    width: 80%;
+  }
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+
+  @media (min-width: 1200px) {
+    width: 50%;
+  }
 `;
+
+export const Icon = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+
+// ... rest of the styled components
