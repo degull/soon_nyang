3822d9716dbe0632f9f974ff0effff0a3a66b060
degull <skytmdgus99@gmail.com>
2024-04-09 17:10:56 +0900

    게시글 profile 출력

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,37 +2,41 @@ import React, { useState, useEffect } from 'react';
 import * as S from './Main.styled';
 import Header from '../Header/Header';
 import Menu from '../Menu/Menu';
-import axios from 'axios'; // axios를 사용하여 API 호출
 
 export default function Main() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await axios.get('http://soonnyang.ap-northeast-2.elasticbeanstalk.com/v1/posts');
-        setPosts(response.data.content); // API 응답에서 게시물 내용(content)만 가져와서 상태 업데이트
-      } catch (error) {
+    // API 호출
+    fetch('http://soonnyang.ap-northeast-2.elasticbeanstalk.com/v1/posts')
+      .then(response => response.json())
+      .then(data => {
+        // API 응답을 받아서 posts 상태 업데이트
+        setPosts(data.content);
+      })
+      .catch(error => {
         console.error('Error fetching posts:', error);
-      }
-    }
-
-    fetchPosts();
-  }, []); // useEffect를 한 번만 호출하기 위해 빈 배열 전달
+      });
+  }, []);
 
   return (
     <S.Wrapper>
       <Header />
-      <div>
+      {/* 게시글 출력 */}
+      <S.PostsContainer>
         {posts.map(post => (
-          <div key={post.postId}>
-            <h2>{post.catDetailResponse.name}</h2> {/* 게시물 제목 */}
-            <p>{post.content}</p> {/* 게시물 내용 */}
-          </div>
+          <S.Post key={post.postId}>
+            <S.PostProfile>
+              <img src="/img/zzon.jpg" alt="User Profile" />
+              <span>{post.catDetailResponse.name}</span>
+              <S.PostNickname>{post.memberDetailResponse.nickname}</S.PostNickname>
+            </S.PostProfile>
+            <S.PostImage src={post.catDetailResponse.imageUrl} alt="Cat" />
+            <div>{post.content}</div>
+          </S.Post>
         ))}
-      </div>
+      </S.PostsContainer>
       <Menu />
     </S.Wrapper>
   );
-  
 }
--- src/components/Main/Main.styled.js
+++ src/components/Main/Main.styled.js
@@ -7,3 +7,69 @@ export const Wrapper = styled.div`
   justify-content: space-between;
   min-height: 100vh;
 `;
+
+export const PostsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+  margin-left: 2px;
+  @media (max-width: 768px) { /* 모바일 화면 */
+    grid-template-columns: repeat(auto-fit, minmax(100%, 1fr));
+  }
+`;
+
+export const Post = styled.div`
+  /* 기존 스타일 코드 */
+  transition: all 0.3s ease;
+  
+  img {
+    width: 100%;
+  }
+
+  div {
+    margin-top: 10px;
+  }
+`;
+
+export const PostProfile = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* 내용을 양쪽 끝으로 정렬 */
+  margin-bottom: 10px;
+  
+
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  span {
+    margin-right: 220px;
+    font-family: 'KCC-Ganpan';
+  @font-face {
+    font-family: 'KCC-Ganpan';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302@1.0/KCC-Ganpan.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+  }
+`;
+
+export const PostImage = styled.img`
+
+`;
+
+export const PostNickname = styled.div`
+ font-size: 14px;
+ font-family: 'KOTRAHOPE';
+ @font-face {
+    font-family: 'KOTRAHOPE';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+text-align: right;
+`;
