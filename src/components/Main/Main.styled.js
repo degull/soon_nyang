2e0de55b4eb3ebd083f397c4b4349ebe3e76a9c3
degull <skytmdgus99@gmail.com>
2024-04-10 23:45:38 +0900

    main 게시글 수정

--- src/components/Main/Main.styled.js
+++ src/components/Main/Main.styled.js
@@ -99,6 +99,13 @@ export const PostLikes = styled.span`
 
 export const PostContent = styled.div`
   font-size: 12px;
+  font-family: 'Ownglyph_meetme-Rg';
+  @font-face {
+    font-family: 'Ownglyph_meetme-Rg';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2402_1@1.0/Ownglyph_meetme-Rg.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
 `;
 
 export const PostBookmark = styled.img`
