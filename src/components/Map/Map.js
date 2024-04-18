8b47c7d3eb86b1c04097ce12cebdc3092ed42a91
degull <skytmdgus99@gmail.com>
2024-04-18 17:07:10 +0900

    최근 위치 10개 조회 api 적용

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,189 +4,110 @@ import Header from '../Header/Header';
 import Menu from '../Menu/Menu';
 
 const Map = () => {
-
     const [isContentOpen, setIsContentOpen] = useState(false);
+    const [catSpots, setCatSpots] = useState([]);
 
     const toggleContent = () => {
         setIsContentOpen(!isContentOpen);
     };
 
-        // 가짜 데이터로 posts 배열 생성
-        const posts = [
-            {
-                postId: 1,
-                catDetailResponse: {
-                    imageUrl: '/img/chuchu.jpg',
-                    name: '고양이1'
-                },
-                memberDetailResponse: {
-                    nickname: '닉네임1'
-                },
-                image: '/img/chuchu.jpg',
-                likeCount: 10,
-                content: '첫 번째 포스트 내용'
-            },
-            {
-                postId: 2,
-                catDetailResponse: {
-                    imageUrl: '/img/coco.jpg',
-                    name: '고양이2'
-                },
-                memberDetailResponse: {
-                    nickname: '닉네임2'
-                },
-                image: '/img/coco.jpg',
-                likeCount: 20,
-                content: '두 번째 포스트 내용'
-            }
-        ];
-
     useEffect(() => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=4d2258a550a5cc13b15f5bcf9bf34124&autoload=false`;
-
-        script.onload = () => {
-            if (window.kakao) {
-                window.kakao.maps.load(() => {
-                    const container = document.getElementById('map');
-                    const options = {
-                        center: new window.kakao.maps.LatLng(36.771939307560515, 126.93139316073213), // 초기 중심 위치 설정
-                        level: 3
-                    };
-                    const map = new window.kakao.maps.Map(container, options);
-
-                    // 첫 번째 마커 이미지 설정
-                    const markerImageSrc = '/img/mark.jpg'; // 사용할 마커 이미지
-                    const markerImageSize = new window.kakao.maps.Size(50, 50); // 마커 이미지 크기
-                    const markerImageOption = { offset: new window.kakao.maps.Point(25, 50) }; // 마커 이미지 옵션
-                    const markerImage = new window.kakao.maps.MarkerImage(markerImageSrc, markerImageSize, markerImageOption);
-
-                    // 두 번째 마커 이미지 설정
-                    const markerImageSrc2 = '/img/mark2.jpg'; // 사용할 두 번째 마커 이미지
-                    const markerImageSize2 = new window.kakao.maps.Size(50, 50); // 두 번째 마커 이미지 크기
-                    const markerImageOption2 = { offset: new window.kakao.maps.Point(25, 50) }; // 두 번째 마커 이미지 옵션
-                    const markerImage2 = new window.kakao.maps.MarkerImage(markerImageSrc2, markerImageSize2, markerImageOption2);
-
-                    // 세 번째 마커 이미지 설정
-                    const markerImageSrc3 = '/img/mark3.jpg'; // 사용할 세 번째 마커 이미지
-                    const markerImageSize3 = new window.kakao.maps.Size(50, 50); // 세 번째 마커 이미지 크기
-                    const markerImageOption3 = { offset: new window.kakao.maps.Point(25, 50) }; // 세 번째 마커 이미지 옵션
-                    const markerImage3 = new window.kakao.maps.MarkerImage(markerImageSrc3, markerImageSize3, markerImageOption3);
-
-                    // 네 번째 마커 이미지 설정
-                    const markerImageSrc4 = '/img/mark4.jpg'; // 사용할 네 번째 마커 이미지
-                    const markerImageSize4 = new window.kakao.maps.Size(50, 50); // 네 번째 마커 이미지 크기
-                    const markerImageOption4 = { offset: new window.kakao.maps.Point(25, 50) }; // 네 번째 마커 이미지 옵션
-                    const markerImage4 = new window.kakao.maps.MarkerImage(markerImageSrc4, markerImageSize4, markerImageOption4);
-
-                    // 첫 번째 마커 추가
-                    const markerPosition1 = new window.kakao.maps.LatLng(36.7713718911621, 126.934133774914); // 첫 번째 마커 위치
-                    // 첫 번째 마커 추가
-                    const marker1 = new window.kakao.maps.Marker({
-                        position: markerPosition1,
-                        image: markerImage, // 첫 번째 마커에 이미지 적용
-                        zIndex: 4, // z-index 설정
-                        strokeWeight: 10, // 테두리 두께 설정
-                        strokeColor: '#000000' // 검정색 테두리 색상 설정
-                    });
-                    marker1.setMap(map);
-
-                    // 두 번째 마커 추가
-                    const markerPosition2 = new window.kakao.maps.LatLng(36.771939307560515, 126.93139316073213); // 두 번째 마커 위치
-                    const marker2 = new window.kakao.maps.Marker({
-                        position: markerPosition2,
-                        image: markerImage2, // 두 번째 마커에 다른 이미지 적용
-                        zIndex: 3 // z-index 설정
-                    });
-                    marker2.setMap(map);
-
-                    // 세 번째 마커 추가
-                    const markerPosition3 = new window.kakao.maps.LatLng(36.769165764637776, 126.93476704606526); // 세 번째 마커 위치
-                    const marker3 = new window.kakao.maps.Marker({
-                        position: markerPosition3,
-                        image: markerImage3, // 세 번째 마커에 다른 이미지 적용
-                        zIndex: 2 // z-index 설정
-                    });
-                    marker3.setMap(map);
-
-                    // 네 번째 마커 추가
-                    const markerPosition4 = new window.kakao.maps.LatLng(36.76878498376117, 126.93067912906109); // 네 번째 마커 위치
-                    const marker4 = new window.kakao.maps.Marker({
-                        position: markerPosition4,
-                        image: markerImage4, // 네 번째 마커에 다른 이미지 적용
-                        zIndex: 1 // z-index 설정
-                    });
-                    marker4.setMap(map);
-                });
-            } else {
-                console.error('Kakao Map SDK is not loaded correctly.');
+        const fetchCatSpots = async () => {
+            try {
+                const response = await fetch('http://soonnyang.ap-northeast-2.elasticbeanstalk.com/v1/cats/5/spots');
+                if (!response.ok) {
+                    throw new Error('고양이 위치를 불러오는 데 실패했습니다.');
+                }
+                const data = await response.json();
+                setCatSpots(data);
+            } catch (error) {
+                console.error('고양이 위치를 불러오는 중 오류가 발생했습니다:', error);
             }
         };
 
-        script.onerror = () => {
-            console.error('Failed to load Kakao Map SDK.');
-        };
-
-        document.head.appendChild(script);
-
-        return () => {
-            if (script.onload) {
-                document.head.removeChild(script);
-            }
-        };
+        fetchCatSpots();
     }, []);
 
+    useEffect(() => {
+        const loadMapScript = () => {
+            const script = document.createElement('script');
+            script.async = true;
+            script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=4d2258a550a5cc13b15f5bcf9bf34124&autoload=false`;
+            script.onload = () => {
+                window.kakao.maps.load(() => {
+                    if (window.kakao.maps) {
+                        initializeMap();
+                    } else {
+                        console.error('Kakao Map SDK is not loaded correctly.');
+                    }
+                });
+            };
+            script.onerror = () => {
+                console.error('카카오 맵 API 스크립트 로드 중 오류가 발생했습니다.');
+            };
+            document.head.appendChild(script);
+        };
+    
+        const initializeMap = () => {
+            const container = document.getElementById('map');
+            if (container) {
+                const options = {
+                    center: new window.kakao.maps.LatLng(36.7713718911650, 126.934133774920),
+                    level: 3,
+                };
+    
+                const map = new window.kakao.maps.Map(container, options);
+    
+                // catSpots 배열을 순회 -> 각 위치에 마커 표시
+                // 디자인 수정
+                catSpots.forEach((spot) => {
+                    const markerPosition = new window.kakao.maps.LatLng(spot.latitude, spot.longitude);
+    
+                    // 마커 생성
+                    const marker = new window.kakao.maps.Marker({
+                        map: map,
+                        position: markerPosition,
+                    });
+    
+                    // 마커 위에 표시할 정보창 생성
+                    // 프로필로 수정
+                    const infowindow = new window.kakao.maps.InfoWindow({
+                        content: `<div>위도: ${spot.latitude}, 경도: ${spot.longitude}</div>`,
+                    });
+    
+                    // 마커를 클릭했을 때 정보창 표시
+                    // 프로필로 수정
+                    window.kakao.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map, marker);
+                    });
+                });
+            } else {
+                console.error('맵 컨테이너를 찾을 수 없습니다.');
+            }
+        };
+    
+        loadMapScript();
+    }, [catSpots]);
+    
+    
+
     return (
-<S.Wrapper>
-  <Header />
-  <div id="map" style={{ width: '100%', height: '250px', marginBottom: '290px'}}></div>
+        <S.Wrapper>
+            <Header />
+            <div id="map" style={{ width: '100%', height: '250px', marginBottom: '290px' }}></div>
 
-  <S.Dropdown src='/img/dropdown.png' onClick={toggleContent}/>
+            <S.Dropdown src='/img/dropdown.png' onClick={toggleContent} />
 
+            {isContentOpen && (
+                <S.ContentContainer>
+                    <S.PostsContainer>
+                        {/* 고양이 위치 &  포스트 목록 렌더링 */}
+                    </S.PostsContainer>
+                </S.ContentContainer>
+            )}
 
-    {isContentOpen && (
-        
-      <S.ContentContainer>
-        
-        <S.PostsContainer>
-        <div style={{ position: 'relative' }}> </div>
-    <S.Catlist>
-      {posts.map(post => (
-        <S.ProfileImage2 key={post.postId} src={post.catDetailResponse.imageUrl} alt="User Profile" />
-      ))}
-    </S.Catlist>
-          {posts.map(post => (
-            <S.Post key={post.postId}>
-              <S.PostProfile>
-                <S.ProfileImage src={post.catDetailResponse.imageUrl} alt="User Profile" />
-                <span>{post.catDetailResponse.name}</span>
-                <S.PostNickname>{post.memberDetailResponse.nickname}</S.PostNickname>
-              </S.PostProfile>
-              <S.PostImage src={post.image} alt="Cat" />
-              <S.PostFooter>
-                <S.PostLikeImg
-                  src='/img/heart_f.png'
-                  alt="Like"
-                  // onClick={() => toggleLike(post.postId)}
-                />
-                <S.PostBookmark
-                  src='/img/bookmark_f.png'
-                  alt="Bookmark"
-                  // onClick={() => toggleBookmark(post.postId)}
-                />
-              </S.PostFooter>
-              <S.PostLikes>좋아요 {post.likeCount}개</S.PostLikes>
-              <S.PostContent>{post.content}</S.PostContent>
-            </S.Post>
-          ))}
-        </S.PostsContainer>
-      </S.ContentContainer>
-    )}
-  
-  <Menu />
-</S.Wrapper>
-
+            <Menu />
+        </S.Wrapper>
     );
 };
 
